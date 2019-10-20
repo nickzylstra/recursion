bcb031d64ea57bca5129f66af765ed7aedfde2f4
nickzylstra <nick.zylstra@gmail.com>
2019-10-20 10:10:58 -0700

    reverts last commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,19 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-let getElementsByClassName = function (className, node = document.body) {
+let getElementsByClassName = function (className) {
   const elements = [];
 
-  if (node.classList && node.classList.contains(className)) {
-    elements.push(node);
-  }
+  const checkNode = function checkNode(node) {
+    if (node.classList && node.classList.contains(className)) {
+      elements.push(node);
+    }
 
-  if (node.childNodes) {
-    node.childNodes.forEach((child) => {
-      const result = getElementsByClassName(className, child);
-      elements.concat(result);
-    });
-  }
+    if (node.childNodes) {
+      node.childNodes.forEach((child) => checkNode(child));
+    }
+  };
+
+  checkNode(document.body);
 
   return elements;
 };
